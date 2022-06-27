2aba07e00065b138ea4fa53bf9d300341c091000
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2022-06-27 22:26:35 +0530

    Repositioned logs;Replaced characters
    - Repositioned the Doactivity of HR in department app,
    - Replaced with proper quotes in HRdepartment

--- Assessment1_OOPsConcept/src/com/hrDepartment/HrDepartment.java
+++ Assessment1_OOPsConcept/src/com/hrDepartment/HrDepartment.java
@@ -3,24 +3,24 @@ package com.hrDepartment;
 import com.superDepartment.SuperDepartment;
 
 public class HrDepartment extends SuperDepartment {
-	
+
 	public HrDepartment() {
 		super();
 	}
-	
-	public String departmentName(){
+
+	public String departmentName() {
 		return "Hr Department";
 	}
-	
+
 	public String getTodaysWork() {
-		return "Fill today’s worksheet and mark your attendance";
+		return "Fill today's worksheet and mark your attendance";
 	}
-	
-	public String getWorkDeadline(){
+
+	public String getWorkDeadline() {
 		return "Complete by EOD";
 	}
-	
-	public String doActivity(){
+
+	public String doActivity() {
 		return "team Lunch";
 	}
 
